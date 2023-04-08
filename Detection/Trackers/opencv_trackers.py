c3f6efe1f0f15fe38b54bca491b64a8dfc2cfe84
Łukasz Główka <glowka36875@gmail.com>
2023-04-08 22:13:22 +0200

    very slow, might not work
    Former-commit-id: 8ca0aa74437460ecd00abc4824fe0aabb257dd92

---
+++ Detection/Trackers/opencv_trackers.py
@@ -0,0 +1,50 @@
+import cv2
+from tracker import Tracker, Track
+import numpy as np
+ 
+ 
+class OpenCVTracker(Tracker):
+    names = {
+        'KCF':cv2.TrackerKCF_create,
+        'MEDIANFLOW':cv2.TrackerMedianFlow_create,
+        'CSRT':cv2.TrackerCSRT_create}
+    
+    def __init__(self, name='CSRT'):
+        self.trackers = cv2.MultiTracker_create()
+        self.tracker_type = self.names[name]
+    
+    def update(self, bboxes, scores, frame):
+        self.update_trackers(bboxes, frame)
+        _, bboxes = self.trackers.update(frame)
+        
+        self.tracks = self.get_bboxes()
+        print(len(self.tracks))
+    
+    def get_bboxes(self):
+        bboxes = []
+        for bbox in self.trackers.getObjects():
+            bbox = bbox.astype(int)
+            bboxes.append(Track(999, bbox))
+        return bboxes
+    
+    def update_trackers(self, bboxes, frame):
+        for new_bbox in bboxes:
+            for bbox in self.trackers.getObjects():
+                bbox = bbox.astype(int)
+                if self.box_over_threshold(new_bbox, bbox):
+                    break
+            tracker = self.tracker_type()
+            self.trackers.add(tracker, frame, new_bbox)
+    
+    def box_over_threshold(self, bbox1, bbox2, threshold = .2):
+        x1, y1, w1, h1 = bbox1
+        x2, y2, w2, h2 = bbox2
+        x_left = max(x1, x2)
+        y_top = max(y1, y2)
+        x_right = min(x1 + w1, x2 + w2)
+        y_bottom = min(y1 + h1, y2 + h2)
+        if x_right < x_left or y_bottom < y_top:
+            return False
+        intersection_area = (x_right - x_left) * (y_bottom - y_top)
+        union_area = w1 * h1 + w2 * h2 - intersection_area
+        return intersection_area/union_area > threshold
