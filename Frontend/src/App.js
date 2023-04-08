7e28c0a1c4fc46dcf4ee8b9be132925dadf36e9a
Hubert <01161897@pw.edu.pl>
2023-04-08 13:34:56 +0200

    mainscreen grid

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -7,9 +7,7 @@ function App() {
   const [isLoggingScreen, setLoggingScreen] = React.useState(true);
   return (
     <div id='app'>
-      <div id='logscreen'>
-        {isLoggingScreen ? <LoggingScreen switchScene = {setLoggingScreen}/> : <MainScreen/>}
-      </div>
+      {isLoggingScreen ? <LoggingScreen switchScene = {setLoggingScreen}/> : <MainScreen/>}
     </div>
   );
 }
--- Frontend/src/MainScreen.js
+++ Frontend/src/MainScreen.js
@@ -2,9 +2,19 @@ import './MainScreen.css'
 
 function MainScreen(props) {
 
+    function pass(){;}
     return (
         <div id="main_screen">
-            This is main screen.
+            <div id="video">A</div>
+            <div id="controls">
+                <button id="slow" onClick={pass}>slow</button>
+                <button id="fast" onClick={pass}>fast</button>
+                <button id="flow" onClick={pass}>flow</button>
+                <input type='range'></input>
+                <button id="download">download</button>
+            </div>
+            <div id="thumbnail_list">C</div>
+            
         </div>
     )
 }
