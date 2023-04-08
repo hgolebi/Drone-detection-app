06278961a3f51d95cd1dd7ff8f11f4743992179d
Hubert <01161897@pw.edu.pl>
2023-04-08 16:07:47 +0200

    video display

--- Frontend/src/MainScreen.js
+++ Frontend/src/MainScreen.js
@@ -5,15 +5,19 @@ function MainScreen(props) {
     function pass(){;}
     return (
         <div id="main_screen">
-            <div id="video">A</div>
-            <div id="controls">
+            <div id="video_container">
+                <video id='video' controls
+                    src='./videos/vid.mp4' type='video/mp4'>
+                </video>
+            </div>
+            <div id="buttons_container">
                 <button id="slow" onClick={pass}>slow</button>
                 <button id="fast" onClick={pass}>fast</button>
                 <button id="flow" onClick={pass}>flow</button>
                 <input type='range'></input>
                 <button id="download">download</button>
             </div>
-            <div id="thumbnail_list">C</div>
+            <div id="thumbnails_container">C</div>
             
         </div>
     )
