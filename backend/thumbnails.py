9bee0ad4235e0021b0fbb830e00ed32ce07c4da2
Hubert <hugo7744@gmail.com>
2023-04-21 15:19:33 +0200

    backend opencv thumbnail generation

--- backend/thumbnails.py
+++ backend/thumbnails.py
@@ -1,19 +1,32 @@
 import os
 import subprocess
+import cv2
 
-UPLOAD_FOLDER = './uploads'
-THUMBNAIL_FOLDER = './thumbnails'
+UPLOAD_FOLDER = './uploads/'
+THUMBNAIL_FOLDER = './thumbnails/'
 
 
 def thumbnail_name(videoname: str):
     return "_".join(videoname.split('.'))+".png"
 
 
-def generate_thumbnail(videoname: str, n=500):
+def generate_thumbnail(videoname: str, size=500):
     thumb_name = thumbnail_name(videoname)
-    cmd = ['ffmpeg', '-y', '-i', f'{UPLOAD_FOLDER}/{videoname}', '-vf', f'thumbnail=n={n}',
-           '-frames:v', '1', f'{THUMBNAIL_FOLDER}/{thumb_name}', '-loglevel', 'quiet']
-    subprocess.run(cmd, shell=False)
+    # cmd = ['ffmpeg', '-y', '-i', f'{UPLOAD_FOLDER}/{videoname}', '-vf', f'thumbnail=n={n}',
+    #        '-frames:v', '1', f'{THUMBNAIL_FOLDER}/{thumb_name}', '-loglevel', 'quiet']
+    # subprocess.run(cmd, shell=False)
+    video = cv2.VideoCapture(UPLOAD_FOLDER + videoname)
+    success, image = video.read()
+    if not success:
+        return False
+    width = image.shape[1]
+    height = image.shape[0]
+    scale_percent = max(height / size, width / size)
+    dim = (int(width / scale_percent), int(height / scale_percent))
+    thumbnail = cv2.resize(image, dim)
+
+    cv2.imwrite(THUMBNAIL_FOLDER + thumb_name, thumbnail)
+    video.release()
 
 
 def check_thumbnail(videname: str):
