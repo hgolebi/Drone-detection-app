64fc1d7bf533e26fce027a2d60f01079a4478790
Łukasz Główka <glowka36875@gmail.com>
2023-06-01 00:34:59 +0200

    fix opencv exception and add id
    Former-commit-id: aacbbaa6c79b8a2e0ec1de1fb90dec1b43490799

--- Detection/Trackers/opencv_trackers.py
+++ Detection/Trackers/opencv_trackers.py
@@ -15,10 +15,17 @@ class OpenCVTracker(Tracker):
         self.trackers = cv2.legacy.MultiTracker_create()
         self.tracker_type = self.names[name]
         self.tracks = []
+        self.tracks_id = []
+        self.id = -1
 
         self.frame_check = 0
         self.last_trackers = np.array([])
 
+    def get_id(self):
+        """ Auto increments id while accessing """
+        self.id += 1
+        return self.id
+    
     def update(self, bboxes, scores, frame):
         """ Update trackers and return new Track objects """
         self.frame_check += 1
@@ -32,12 +39,13 @@ class OpenCVTracker(Tracker):
         self.tracks = self.get_bboxes()
 
     def update_trackers(self, bboxes, frame):
-        """Check IOU of newly detected bboxes """
+        "" "Check IOU of newly detected bboxes """
         current_boxes = self.trackers.getObjects()
         for new_bbox in bboxes:
-            if len(current_boxes) == 0 or not self.bbox_over_threshold(new_bbox, current_boxes):
+            if len(current_boxes) == 0 or not self.bbox_over_IOU_threshold(new_bbox, current_boxes):
                 tracker = self.tracker_type()
                 self.trackers.add(tracker, frame, new_bbox)
+                self.tracks_id.append(self.get_id())
 
     def remove_unused(self, frame):
         """ Remove unused trackers, based on lack of movement """
@@ -46,11 +54,14 @@ class OpenCVTracker(Tracker):
         arr2_shape = self.last_trackers.shape[0]
 
         if arr1_shape > 0 and arr2_shape > 0 and arr1_shape == arr2_shape:
-            comparison = np.all(np.isclose(trackers_arr, self.last_trackers, atol=.1), axis=1)
-            idx = np.where(comparison, 1, 0)
+            lost_objects = np.all(np.isclose(trackers_arr, self.last_trackers, atol=.1), axis=1)
+            object_is_zero = np.all(trackers_arr == 0, axis=1)
+            lost_objects = lost_objects | object_is_zero
+            
+            self.tracks_id = [id for id, lost in zip(self.tracks_id, lost_objects) if lost == False]
             self.trackers = cv2.legacy.MultiTracker_create()
-            for i, tracker_value in zip(idx, trackers_arr):
-                if i == 0:
+            for lost, tracker_value in zip(lost_objects, trackers_arr):
+                if lost == False:
                     tracker = self.tracker_type()
                     self.trackers.add(tracker, frame, tuple(tracker_value))
 
@@ -63,9 +74,8 @@ class OpenCVTracker(Tracker):
         if not bboxes.any():
             return []
 
-        # TODO proper id
         bboxes = self.xywh_to_xyxy(bboxes)
-        for i, bbox in enumerate(bboxes):
+        for id, bbox in zip(self.tracks_id, bboxes):
             bbox = bbox.astype(int)
-            tracks.append(Track(i, bbox))
+            tracks.append(Track(id, bbox))
         return tracks
