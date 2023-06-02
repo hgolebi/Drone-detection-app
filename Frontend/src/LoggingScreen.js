289c3fc1de0a35578458dcab89db87b92f1999d9
hgolebi <hugo7744@gmail.com>
2023-06-02 22:40:45 +0200

    logging screen visuals
    Former-commit-id: 4cd93ec19bb7e7877c1318722b42845e9202921a

--- Frontend/src/LoggingScreen.js
+++ Frontend/src/LoggingScreen.js
@@ -62,15 +62,15 @@ class LoggingScreen extends React.Component {
 
     render () {
         return (
-            <div id="logging_screen">
-                <header id='log_head'>LOG IN</header>
-                <label id="username_label" >username:</label>
-                <input type='text' id='username_input' onChange={this.handleLoginChange}></input>
-                <label id="pass_label" >password:</label>
-                <input type='password' id='pass_input' onChange={this.handlePasswordChange}></input>
-                <div id="log_buttons">
-                    <button id='username_button' onClick={() => this.logIn()}>Log in</button>
-                    <button id='register_button' onClick={() => this.register()}>Register</button>
+            <div className="logging_screen">
+                <header className='log_head'>LOG IN</header>
+                <label className="label" >username:</label>
+                <input type='text' className='input' onChange={this.handleLoginChange}></input>
+                <label className="label" >password:</label>
+                <input type='password' className='input' onChange={this.handlePasswordChange}></input>
+                <div className="log_buttons">
+                    <button className='button' onClick={() => this.logIn()}>Log in</button>
+                    <button className='button' onClick={() => this.register()}>Register</button>
                 </div>
             </div>
         )
